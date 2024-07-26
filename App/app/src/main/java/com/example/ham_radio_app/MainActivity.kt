1c21d66783073fe172d2ba82918df803f6a73a2d
Lukas <lukas.neuhauser@edu.htl-klu.at>
2024-07-26 19:14:38 +0200

    adding Bluetooth Permission 0.3

--- App/app/src/main/java/com/example/ham_radio_app/MainActivity.kt
+++ App/app/src/main/java/com/example/ham_radio_app/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.ham_radio_app
 import android.Manifest
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothAdapter.getDefaultAdapter
 import android.bluetooth.BluetoothDevice
 import android.content.BroadcastReceiver
 import android.content.Context
@@ -11,70 +10,31 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.os.Build
-import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.ham_radio_app.ui.theme.HAMRadioAppTheme
 
 class MainActivity : ComponentActivity() {
 
-    private val PERMISSION_CODE = 1
-    private val bluetoothAdapter: BluetoothAdapter = getDefaultAdapter()
-
-    private val activityResultLauncher = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
-    ) { result ->
-        if (result.resultCode == RESULT_OK) {
-            Log.i("Bluetooth", ":request permission result ok")
-        } else {
-            Log.i("Bluetooth", ":request permission result canceled / denied")
-        }
-    }
-
-    private fun requestBluetoothPermission() {
-        val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-        activityResultLauncher.launch(enableBluetoothIntent)
-    }
-
-    @SuppressLint("MissingPermission")
-    var pairedDevices: Set<BluetoothDevice> = bluetoothAdapter.bondedDevices
-    var discoveredDevices: Set<BluetoothDevice> = emptySet()
+    private var bluetoothAdapter: BluetoothAdapter? = null
+    private var pairedDevices: Set<BluetoothDevice> = emptySet()
+    private var discoveredDevices: Set<BluetoothDevice> = emptySet()
 
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent) {
@@ -83,76 +43,58 @@ class MainActivity : ComponentActivity() {
                     val device: BluetoothDevice? =
                         intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
                     if (device != null) {
-                        val updated = discoveredDevices.plus(device)
-                        discoveredDevices = updated
+                        discoveredDevices = discoveredDevices.plus(device)
                     }
-                    Log.i("Bluetooth", "onRecieve: Device found")
+                    Log.i("Bluetooth", "onReceive: Device found")
                 }
 
                 BluetoothAdapter.ACTION_DISCOVERY_STARTED -> {
-                    Log.i("Bluetooth", "onRecieve: Started Discovery")
+                    Log.i("Bluetooth", "onReceive: Started Discovery")
                 }
 
                 BluetoothAdapter.ACTION_DISCOVERY_FINISHED -> {
-                    Log.i("Bluetooth", "onRecieve: Finished Discovery")
+                    Log.i("Bluetooth", "onReceive: Finished Discovery")
                 }
             }
         }
     }
 
-    @SuppressLint("MissingPermission")
-    @RequiresApi(Build.VERSION_CODES.M)
-    fun scan(): Set<BluetoothDevice> {
-        if (bluetoothAdapter.isDiscovering) {
-            bluetoothAdapter.cancelDiscovery()
-            bluetoothAdapter.startDiscovery()
-        } else {
-            bluetoothAdapter.startDiscovery()
+    private fun scan(): Set<BluetoothDevice> {
+        bluetoothAdapter?.let { adapter ->
+            if (adapter.isDiscovering) {
+                adapter.cancelDiscovery()
+            }
+            adapter.startDiscovery()
+            Handler(Looper.getMainLooper()).postDelayed({
+                adapter.cancelDiscovery()
+            }, 10000L)
         }
-        Handler(Looper.getMainLooper()).postDelayed({
-            bluetoothAdapter.cancelDiscovery()
-        }, 10000L)
         return discoveredDevices
     }
 
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     @OptIn(ExperimentalMaterial3Api::class)
-    @RequiresApi(Build.VERSION_CODES.M)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
 
         val foundFilter = IntentFilter(BluetoothDevice.ACTION_FOUND)
         val startFilter = IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_STARTED)
         val endFilter = IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
-        registerReceiver(receiver, foundFilter);
-        registerReceiver(receiver, startFilter);
-        registerReceiver(receiver, endFilter);
-
-        if (!bluetoothAdapter.isEnabled) {
-            requestBluetoothPermission()
-        }
-
-        if (SDK_INT >= Build.VERSION_CODES.O) {
-            if (ContextCompat.checkSelfPermission(
-                    baseContext, android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                ActivityCompat.requestPermissions(
-                    this, arrayOf(android.Manifest.permission.ACCESS_BACKGROUND_LOCATION),
-                    PERMISSION_CODE
-                )
-            }
-        }
+        registerReceiver(receiver, foundFilter)
+        registerReceiver(receiver, startFilter)
+        registerReceiver(receiver, endFilter)
 
         setContent {
             var devices: Set<BluetoothDevice> by remember { mutableStateOf(emptySet()) }
+            var hasPermission by remember { mutableStateOf(false) }
 
             HAMRadioAppTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
                     Scaffold(topBar = {
                         TopAppBar(
                             title = {
@@ -170,33 +112,48 @@ class MainActivity : ComponentActivity() {
                                 .padding(top = 20.dp),
                             horizontalAlignment = Alignment.CenterHorizontally
                         ) {
-                            Button(onClick = { devices = scan() }) {
+                            if (hasPermission) {
+                                Button(onClick = { devices = scan() }) {
+                                    Text(
+                                        text = "Scan",
+                                        style = MaterialTheme.typography.headlineMedium
+                                    )
+                                }
+                                Spacer(modifier = Modifier.height(10.dp))
                                 Text(
-                                    text = "Scan",
-                                    style = MaterialTheme.typography.headlineMedium
+                                    text = "Paired Devices",
+                                    style = MaterialTheme.typography.headlineLarge
                                 )
-                            }
-                            Spacer(modifier = Modifier.height(10.dp))
-                            Text(
-                                text = "Paired Devices",
-                                style = MaterialTheme.typography.headlineLarge
-                            )
-                            Spacer(modifier = Modifier.height(10.dp))
-                            pairedDevices.forEach { device ->
-                                Card(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(horizontal = 10.dp, vertical = 5.dp)
-                                    //elevation = 10.dp
-                                ) {
-                                    Column(
+                                Spacer(modifier = Modifier.height(10.dp))
+                                pairedDevices.forEach { device ->
+                                    Card(
                                         modifier = Modifier
                                             .fillMaxWidth()
-                                            .padding(10.dp),
-                                        verticalArrangement = Arrangement.spacedBy(10.dp)
+                                            .padding(horizontal = 10.dp, vertical = 5.dp)
                                     ) {
-                                        Text(text = device.name)
-                                        Text(text = device.address)
+                                        Column(
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .padding(10.dp),
+                                            verticalArrangement = Arrangement.spacedBy(10.dp)
+                                        ) {
+                                            Text(text = device.name ?: "Unknown Device")
+                                            Text(text = device.address)
+                                        }
+                                    }
+                                }
+                            } else {
+                                RequestBluetoothPermission { granted ->
+                                    hasPermission = granted
+                                    if (granted) {
+                                        bluetoothAdapter?.let { adapter ->
+                                            if (!adapter.isEnabled) {
+                                                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                                                startActivityForResult(enableBtIntent, 1)
+                                            } else {
+                                                pairedDevices = adapter.bondedDevices
+                                            }
+                                        }
                                     }
                                 }
                             }
@@ -207,19 +164,55 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    @SuppressLint("MissingPermission")
+    @Composable
+    fun RequestBluetoothPermission(onPermissionResult: (Boolean) -> Unit) {
+        val context = LocalContext.current
+        val requiredPermissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            listOf(
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.BLUETOOTH_SCAN,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        } else {
+            listOf(
+                Manifest.permission.BLUETOOTH,
+                Manifest.permission.BLUETOOTH_ADMIN,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        }
+
+        val allPermissionsGranted = requiredPermissions.all { permission ->
+            ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
+        }
+
+        val permissionLauncher = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            val granted = permissions.values.all { it }
+            onPermissionResult(granted)
+        }
+
+        LaunchedEffect(key1 = Unit) {
+            if (allPermissionsGranted) {
+                onPermissionResult(true)
+            } else {
+                permissionLauncher.launch(requiredPermissions.toTypedArray())
+            }
+        }
+    }
+
     override fun onDestroy() {
         super.onDestroy()
-        if  (bluetoothAdapter.isDiscovering) bluetoothAdapter.cancelDiscovery()
+        bluetoothAdapter?.let { adapter ->
+            if (adapter.isDiscovering) adapter.cancelDiscovery()
+        }
         unregisterReceiver(receiver)
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     HAMRadioAppTheme {
-
     }
 }
