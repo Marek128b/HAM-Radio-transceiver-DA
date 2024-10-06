174e0fc2221b4f8d7b35ce9dce8bf9ceef043f7c
Lukas <lukas.neuhauser@edu.htl-klu.at>
2024-10-06 21:08:58 +0200

    adding Bluetooth Permission 1.0

---
+++ FUNKY-App/app/src/main/java/com/example/myapplication/presentation/BluetoothUiState.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.presentation
+
+import com.example.myapplication.domain.chat.BluetoothDevice
+
+data class BluetoothUiState(
+    val scannedDevices: List<BluetoothDevice> = emptyList(),
+    val pairedDevices: List<BluetoothDevice> = emptyList(),
+)
