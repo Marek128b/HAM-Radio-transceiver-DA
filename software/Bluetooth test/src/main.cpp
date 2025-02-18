2dd94c04d01464cda2c3c90c3149b62a3b66b914
Lukas Alexander Neuhauser (5AHEL) <lukas.neuhauser@edu.htl-klu.at>
2025-02-18 08:11:24 +0100

    Real Json

--- software/Bluetooth test/src/main.cpp
+++ software/Bluetooth test/src/main.cpp
@@ -30,10 +30,12 @@ void loop()
   if (millis() - lastMillis >= interval)
   {
     doc.clear();
-    doc["voltage"] = round((3*(3.7+((float)random(4)/(float)10))) * 1000.0) / 1000.0;;
+    doc["op"] = false; 
     doc["frequency"] = 14.122100;
-    doc["call"] = "OE8GKE";
+    doc["voltage"] = round((3*(3.7+((float)random(4)/(float)10))) * 1000.0) / 1000.0;;
     doc["name"] = "Georg";
+    doc["call"] = "OE8GKE";
+    doc["temperature"] = 30.3;
     String out;
     serializeJson(doc, out);
     //Serial.println(out);
